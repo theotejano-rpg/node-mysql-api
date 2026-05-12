48806b03fe907b679c583b5fd1996b8b79b2a578
Your Name <you@example.com>
2026-05-12 12:48:27 +0800

    For testing na

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -15,7 +15,7 @@ async function initialize() {
 
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, { host, port, dialect: 'mysql' });
 
     db.Account = accountModel(sequelize);
     db.RefreshToken = refreshTokenModel(sequelize);
