bb1b00899521ae35c93b891900941346935877e7
Your Name <you@example.com>
2026-05-16 10:04:45 +0800

    token

--- _helpers/db.ts
+++ _helpers/db.ts
@@ -1,4 +1,3 @@
-import config from '../config.json';
 import mysql from 'mysql2/promise';
 import { Sequelize } from 'sequelize';
 import accountModel from '../accounts/account.model';
@@ -10,9 +9,13 @@ export default db;
 initialize();
 
 async function initialize() {
-    const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
+    const host = process.env.DB_HOST!;
+    const port = parseInt(process.env.DB_PORT || '3306');
+    const user = process.env.DB_USER!;
+    const password = process.env.DB_PASSWORD!;
+    const database = process.env.DB_NAME!;
 
+    const connection = await mysql.createConnection({ host, port, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     const sequelize = new Sequelize(database, user, password, { host, port, dialect: 'mysql' });
